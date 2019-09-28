8fdbf2d403a6d6b21fb7bf062dd26432b6234725
Caroline Guirgis <cguirgis@nd.edu>
2019-09-27 21:35:42 -0400

    more changes 9.27

--- project_one/assets/toggle.jquery.js
+++ project_one/assets/toggle.jquery.js
@@ -1,8 +1,20 @@
 $(document).ready(function (){
     //ID or Class name of the button goes into the first section
-    $('#button').click(function() {
+    $('#button-off').click(function() {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $("#letter-analysis").toggleClass("jordan");
+        $(".large-letter-group h1").toggleClass("glow");
+          $("#button-off").toggleClass("jordan");
+        });
+});
+
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#button-on').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#letter-analysis").toggleClass("jordan");
+        $("#button-off").toggleClass("jordan");
+          $("#button-on").toggleClass("jordan");
         $(".large-letter-group h1").toggleClass("glow");
         });
 });
